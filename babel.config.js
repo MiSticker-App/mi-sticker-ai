660e2e631d6c162a061078b0257fd5dfedebebfd
ahorify-ai <ahorify@gmail.com>
2025-12-21 20:39:15 +0100

    Downgrade AdMob to 14.2.2 and fix Babel

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // NativeWind para los estilos
       "nativewind/babel",
-      // Reanimated siempre debe ir el último
       "react-native-reanimated/plugin",
     ],
   };
