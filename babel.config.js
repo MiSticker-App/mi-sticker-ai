10dc959a3b3589d5d2ba6b8b22f01054ed33df9a
ahorify-ai <ahorify@gmail.com>
2025-12-21 20:18:56 +0100

    Fix AdMob Kotlin error and Babel syntax

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,12 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel'],
+    plugins: [
+      // NativeWind para los estilos
+      "nativewind/babel",
+      // Reanimated siempre debe ir el último
+      "react-native-reanimated/plugin",
+    ],
   };
 };
-
